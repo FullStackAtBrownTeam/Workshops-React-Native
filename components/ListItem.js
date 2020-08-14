bf3f66e4bace226980c1be278c567a895747ddf0
lisyu <stella_li@brown.edu>
2020-08-13 22:20:48 -0400

    list dynamically adds items, style tweaks

--- components/ListItem.js
+++ components/ListItem.js
@@ -4,12 +4,11 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 export default function ListItem(props) {
 
     // State variable that tracks if this to-do item is marked as done
-    // TODO: use prop?
-    const [isDone, setIsDone] = useState(false);
+    const [isDone, setIsDone] = useState(props.isDone? props.isDone : false);
 
     return(
-        <View style={styles.container}>
-            <Text style={styles.itemText}>{props.text}</Text>
+        <View style={[styles.container, isDone? styles.isDoneContainer : undefined]}>
+            <Text style={[styles.itemText, isDone? styles.isDoneText : undefined]}>{props.text}</Text>
             <Button
                 onPress={() => setIsDone(!isDone)}
                 title={isDone? "Mark Undone" : "Mark Done"}
@@ -24,14 +23,27 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-        marginLeft: '5%',
-        marginRight: '5%',
+        margin: '2%',
+        paddingLeft: '5%',
+        paddingRight: '5%',
+        paddingTop: '3%',
+        paddingBottom: '3%',
+
+        backgroundColor: '#e9e9e9'
     },
     itemText: {
         marginLeft: '3%',
         marginRight: '3%',
         fontSize: 20,
         flex: 1,
-        flexWrap: 'wrap'
+        flexWrap: 'wrap',
+
+        color: '#000000'
+    },
+    isDoneContainer: {
+        backgroundColor: '#ffffff'
+    },
+    isDoneText: {
+        color: '#cccccc'
     }
 });
--- components/ListScreen.js
+++ components/ListScreen.js
@@ -1,44 +1,100 @@
-import React, { Fragment } from 'react';
-import { StyleSheet, View, ScrollView, TextInput } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, ScrollView, TextInput, Button } from 'react-native';
 import Header from './Header';
 import ListItem from './ListItem'
 import { useState, useEffect } from "react";
 
-export default function ListScreen() {
-    const [inputText, setText] = useState("");
-    const [submitted, setSubmitted] = useState(false);
+class ListScreen extends Component {
+    // const [inputText, setText] = useState("");
+    // const [items, setItems] = useState([]);
 
-    const onSubmit = () => {
-        console.log(inputText)
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            inputText: null,
+            showDone: true
+        };
     }
 
-    return (
-        <View style={styles.container}>
-            <Header title="To Do List" />
-            <TextInput
-                placeholder="Add Item"
-                onChangeText={text => setText(text)}
-                style={styles.textInput}
-                onSubmitEditing={() => onSubmit()}
-            />
-        </View>
-    );
+    setText(text) {
+        this.setState({
+            inputText: text
+        });
+    }
+
+    addTaskItem(taskName) {
+        if (this.state.inputText !== "") {
+            this.setState({
+                items: this.state.items.concat(
+                    <ListItem text={taskName} isDone={false} />
+                ),
+                inputText: ""
+            });
+        }
+    }
+
+    toggleShowDone() {
+        this.setState({
+            showDone: this.state.showDone
+        });
+    }
+
+    renderItems() {
+        let items = this.state.items;
+        if (this.state.hideDone) {
+            items = items.filter(item => {
+                console.log(item.state);
+                item.state.isDone
+            });
+        }
+        return this.state.items;
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Header title="To Do List" />
+                {/* <Button 
+                    onPress={() => this.toggleShowDone()}
+                    title="Toggle Show Done"
+                /> */}
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Add Item"
+                    value={this.state.inputText}
+                    onChangeText={text => this.setText(text)}
+                    onSubmitEditing={() => this.addTaskItem(this.state.inputText)}
+                />
+                <ScrollView style={styles.itemContainer}>
+                    {this.renderItems()}
+                </ScrollView>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop: 45,
+        paddingTop: 45
     },
     textInput: {
         flexDirection: "row",
         alignItems: "center",
-        borderColor: "black",
+        borderColor: "#cccccc",
         borderWidth: 3,
         paddingVertical: 5,
         paddingHorizontal: 14,
         fontSize: 18,
         color: "#666666",
-        marginBottom: 15
+        margin: 10
     },
+    itemContainer: {
+        flex: 1,
+        flexDirection: "column"
+        // justifyContent: "flex-start"
+    }
 });
+
+export default ListScreen;
